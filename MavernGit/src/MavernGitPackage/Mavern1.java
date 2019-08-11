70352f37b3a02a00048eb4d204ffdc5aa1935fda
Madhavi-Kavya <https://github.com/Madhavi-Kavya/Selenium.git>
2019-08-11 03:18:16 -0500

    MavernGit One

---
+++ MavernGit/src/MavernGitPackage/Mavern1.java
@@ -0,0 +1,13 @@
+package MavernGitPackage;
+
+public class Mavern1 {
+	public void mavernONE()
+	{
+		System.out.println("Mavern One Class");
+	}
+	public void mavernTWO()
+	{
+		System.out.println("Mavern Two Class");
+	}
+
+}
